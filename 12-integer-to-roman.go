40652e620c9fdc8431cb83d34677de08b8e70251
ZHOU Cheng <c.zhou@live.com>
2020-03-01 20:17:31 +0800

    Create 12-integer-to-roman.go

---
+++ 12-integer-to-roman.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type romanSymbol struct {
+	Symbol string
+	Value  int
+}
+
+func intToRoman(num int) string {
+	symbols := []romanSymbol{
+		{Symbol: "M", Value: 1000},
+		{Symbol: "CM", Value: 900},
+		{Symbol: "D", Value: 500},
+		{Symbol: "CD", Value: 400},
+		{Symbol: "C", Value: 100},
+		{Symbol: "XC", Value: 90},
+		{Symbol: "L", Value: 50},
+		{Symbol: "XL", Value: 40},
+		{Symbol: "X", Value: 10},
+		{Symbol: "X", Value: 10},
+		{Symbol: "IX", Value: 9},
+		{Symbol: "V", Value: 5},
+		{Symbol: "IV", Value: 4},
+		{Symbol: "I", Value: 1},
+	}
+	var result string
+	for _, symbol := range symbols {
+		for ; num >= symbol.Value; num -= symbol.Value {
+			result += symbol.Symbol
+		}
+	}
+	return result
+}
+
+func main() {
+	// Output: "MCMXCIV"
+	fmt.Println(intToRoman(1994))
+}
