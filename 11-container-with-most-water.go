179d981b90ae516e78f15ee96a1a46f267426fcc
ZHOU Cheng <c.zhou@live.com>
2020-03-01 20:03:41 +0800

    Update 11-container-with-most-water.go

--- 11-container-with-most-water.go
+++ 11-container-with-most-water.go
@@ -10,41 +10,16 @@ func Min(x, y int) int {
 }
 
 func maxArea(height []int) int {
-	heightBefore := make(map[int]int)
-	heightAfter := make(map[int]int)
-	submerged := make(map[int]bool)
-
-	for i, j := 1, height[0]; i < len(height); i++ {
-		heightBefore[i] = j
-		if j < height[i] {
-			j = height[i]
+	var result int
+	for i, j := 0, len(height)-1; i < j; {
+		area := Min(height[i], height[j]) * (j - i)
+		if result < area {
+			result = area
 		}
-	}
-	for i, j := len(height)-2, height[len(height)-1]; i >= 0; i-- {
-		heightAfter[i] = j
-		if j < height[i] {
-			j = height[i]
-		}
-	}
-	for i, j := 1, len(height)-2; i <= j; i++ {
-		if heightBefore[i] >= height[i] && height[i] <= heightAfter[i] {
-			submerged[i] = true
-		}
-	}
-
-	result := 0
-	for i := 0; i < len(height)-1; i++ {
-		if submerged[i] {
-			continue
-		}
-		for j := i + 1; j < len(height); j++ {
-			if submerged[j] {
-				continue
-			}
-			area := Min(height[i], height[j]) * (j - i)
-			if result < area {
-				result = area
-			}
+		if height[i] < height[j] {
+			i++
+		} else {
+			j--
 		}
 	}
 	return result
