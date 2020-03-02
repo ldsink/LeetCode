54bed34e01065e31d49358ed582bd05042856a5c
ZHOU Cheng <c.zhou@live.com>
2020-03-02 22:09:51 +0800

    Create 17-letter-combinations-of-a-phone-number.go

---
+++ 17-letter-combinations-of-a-phone-number.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+var NumRunes = map[rune][]rune{
+	'2': []rune("abc"),
+	'3': []rune("def"),
+	'4': []rune("ghi"),
+	'5': []rune("jkl"),
+	'6': []rune("mno"),
+	'7': []rune("pqrs"),
+	'8': []rune("tuv"),
+	'9': []rune("wxyz"),
+}
+
+func _processCombine(rs []rune, prefix []rune) []string {
+	if len(rs) == 0 {
+		return []string{string(prefix)}
+	}
+	var result []string
+	for _, r := range NumRunes[rs[0]] {
+		current := _processCombine(rs[1:], append(prefix, r))
+		result = append(result, current...)
+	}
+	return result
+}
+
+func letterCombinations(digits string) []string {
+	var result []string
+	rs := []rune(digits)
+	if len(rs) == 0 {
+		return result
+	}
+	var prefix []rune
+	current := _processCombine(rs, prefix)
+	result = append(result, current...)
+	return result
+}
+
+func main() {
+	fmt.Println(letterCombinations("23"))
+}
