1a4bb26cc0c9953b01786a0dcc857c809e1af464
ZHOU Cheng <c.zhou@live.com>
2020-03-03 21:45:49 +0800

    Create 21-merge-two-sorted-lists.go

---
+++ 21-merge-two-sorted-lists.go
@@ -0,0 +1,47 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	} else if l2 == nil {
+		return l1
+	}
+	var result *ListNode
+	if (*l1).Val > (*l2).Val {
+		result = l2
+		l2 = (*l2).Next
+	} else {
+		result = l1
+		l1 = (*l1).Next
+	}
+	current := result
+	for ; l1 != nil && l2 != nil; {
+		if (*l1).Val > (*l2).Val {
+			(*current).Next = l2
+			current = l2
+			l2 = (*l2).Next
+		} else {
+			(*current).Next = l1
+			current = l1
+			l1 = (*l1).Next
+		}
+	}
+	if l1 != nil {
+		(*current).Next = l1
+	} else if l2 != nil {
+		(*current).Next = l2
+	}
+	return result
+}
