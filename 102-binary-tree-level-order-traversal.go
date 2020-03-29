5e0a4fc094b57bf17a6a67e7efcda7a377807d2b
ZHOU Cheng <c.zhou@live.com>
2020-03-29 17:09:17 +0800

    Simplify code

--- 102-binary-tree-level-order-traversal.go
+++ 102-binary-tree-level-order-traversal.go
@@ -1,17 +1,16 @@
 package main
 
-type NodeWithLevel struct {
-	node  *TreeNode
-	level int
-}
-
 func levelOrder(root *TreeNode) [][]int {
 	var result [][]int
 	if root == nil {
 		return result
 	}
 
-	stack := []NodeWithLevel{NodeWithLevel{node: root, level: 0}}
+	type Pair struct {
+		node  *TreeNode
+		level int
+	}
+	stack := []Pair{{node: root, level: 0}}
 	for ; len(stack) > 0; {
 		nodeWithLevel := stack[0]
 		stack = stack[1:]
@@ -21,10 +20,10 @@ func levelOrder(root *TreeNode) [][]int {
 		}
 		result[level] = append(result[level], node.Val)
 		if node.Left != nil {
-			stack = append(stack, NodeWithLevel{node: node.Left, level: level + 1})
+			stack = append(stack, Pair{node: node.Left, level: level + 1})
 		}
 		if node.Right != nil {
-			stack = append(stack, NodeWithLevel{node: node.Right, level: level + 1})
+			stack = append(stack, Pair{node: node.Right, level: level + 1})
 		}
 	}
 	return result
--- 103-binary-tree-zigzag-level-order-traversal.go
+++ 103-binary-tree-zigzag-level-order-traversal.go
@@ -1,17 +1,17 @@
 package main
 
-type Node103 struct {
-	node  *TreeNode
-	level int
-}
-
 func zigzagLevelOrder(root *TreeNode) [][]int {
 	var result [][]int
 	if root == nil {
 		return result
 	}
 
-	stack := []Node103{Node103{node: root, level: 0}}
+	type Pair struct {
+		node  *TreeNode
+		level int
+	}
+
+	stack := []Pair{{node: root, level: 0}}
 	for ; len(stack) > 0; {
 		nodeWithLevel := stack[0]
 		stack = stack[1:]
@@ -21,10 +21,10 @@ func zigzagLevelOrder(root *TreeNode) [][]int {
 		}
 		result[level] = append(result[level], node.Val)
 		if node.Left != nil {
-			stack = append(stack, Node103{node: node.Left, level: level + 1})
+			stack = append(stack, Pair{node: node.Left, level: level + 1})
 		}
 		if node.Right != nil {
-			stack = append(stack, Node103{node: node.Right, level: level + 1})
+			stack = append(stack, Pair{node: node.Right, level: level + 1})
 		}
 	}
 
