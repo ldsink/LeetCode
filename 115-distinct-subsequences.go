5b5af46d7398987c4b39d07cf372311e05bc69cc
ZHOU Cheng <c.zhou@live.com>
2020-03-29 16:14:58 +0800

    Update 115-distinct-subsequences.go

--- 115-distinct-subsequences.go
+++ 115-distinct-subsequences.go
@@ -1,45 +1,25 @@
 package main
 
-var (
-	T string
-	S string
-)
-
-func dynamicSearch(si, ti int, saved [][]int) {
-	if saved[si][ti] != -1 {
-		return
-	}
-	saved[si][ti] = 0
-	if len(T) == ti {
-		saved[si][ti] = 1
-		return
-	} else if len(S[si:]) < len(T[ti:]) {
-		return
-	} else if len(S[si:]) == len(T[ti:]) {
-		if S[si:] == T[ti:] {
-			saved[si][ti] = 1
-		}
-		return
-	}
-	dynamicSearch(si+1, ti, saved)
-	saved[si][ti] += saved[si+1][ti]
-	if S[si] == T[ti] {
-		dynamicSearch(si+1, ti+1, saved)
-		saved[si][ti] += saved[si+1][ti+1]
-	}
-}
-
 func numDistinct(s string, t string) int {
-	S, T = s, t
-	m, n := len(s)+1, len(t)+1
 	var saved [][]int
-	for i := 0; i < m; i++ {
+	for i := 0; i < 2; i++ {
 		var line []int
-		for j := 0; j < n; j++ {
-			line = append(line, -1)
+		for j := 0; j < len(t)+1; j++ {
+			line = append(line, 0)
 		}
 		saved = append(saved, line)
 	}
-	dynamicSearch(0, 0, saved)
+	m, n := len(s), len(t)
+	saved[m%2][n] = 1
+	for i := m - 1; i >= 0; i-- {
+		k, l := i%2, (i+1)%2
+		saved[k][n] = 1
+		for j := n - 1; j >= 0; j-- {
+			saved[k][j] = saved[l][j]
+			if s[i] == t[j] {
+				saved[k][j] += saved[l][j+1]
+			}
+		}
+	}
 	return saved[0][0]
 }
