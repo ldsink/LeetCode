a57d11853e42ab59150d4e7f429f44d18623ae69
ZHOU Cheng <c.zhou@live.com>
2020-04-12 22:11:22 +0800

    Update 135-candy.go

--- 135-candy.go
+++ 135-candy.go
@@ -5,7 +5,7 @@ func candy(ratings []int) int {
 		return len(ratings)
 	}
 	leftLow := make([]int, len(ratings))
-	righLow := make([]int, len(ratings))
+	rightLow := make([]int, len(ratings))
 	for i := 1; i < len(ratings); i++ {
 		if ratings[i] > ratings[i-1] {
 			leftLow[i] = leftLow[i-1] + 1
@@ -13,13 +13,13 @@ func candy(ratings []int) int {
 	}
 	for i := len(ratings) - 2; i >= 0; i-- {
 		if ratings[i] > ratings[i+1] {
-			righLow[i] = righLow[i+1] + 1
+			rightLow[i] = rightLow[i+1] + 1
 		}
 	}
 	var candies int
 	for i := 0; i < len(ratings); i++ {
-		if leftLow[i] < righLow[i] {
-			candies += 1 + righLow[i]
+		if leftLow[i] < rightLow[i] {
+			candies += 1 + rightLow[i]
 		} else {
 			candies += 1 + leftLow[i]
 		}
