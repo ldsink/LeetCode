89a64a057b694865e849a9a58fca2541e534651e
ZHOU Cheng <c.zhou@live.com>
2020-04-12 23:05:10 +0800

    Update 137-single-number-ii.go

--- 137-single-number-ii.go
+++ 137-single-number-ii.go
@@ -8,15 +8,12 @@ func singleNumber(nums []int) int {
 	var result int
 
 	bitLen := uint(unsafe.Sizeof(result) << 3)
-	one := make([]int, bitLen)
 	zero := make([]int, bitLen)
 
 	var i uint
 	for _, num := range nums {
 		for i = 0; i < bitLen; i++ {
-			if (num>>i)&1 == 1 {
-				one[i] ++
-			} else {
+			if (num>>i)&1 == 0 {
 				zero[i] ++
 			}
 		}
