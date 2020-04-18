cf6fc613c3d7ae89715873e798d6eac178c87376
ZHOU Cheng <c.zhou@live.com>
2020-04-18 14:18:09 +0800

    Create 145-binary-tree-postorder-traversal.go

---
+++ 145-binary-tree-postorder-traversal.go
@@ -0,0 +1,28 @@
+package main
+
+func postorderTraversal(root *TreeNode) []int {
+	var result []int
+	if root == nil {
+		return result
+	}
+	stack := []*TreeNode{root}
+	for ; len(stack) > 0; {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if node.Left == nil && node.Right == nil {
+			result = append(result, node.Val)
+			continue
+		}
+
+		stack = append(stack, node)
+		if node.Right != nil {
+			stack = append(stack, node.Right)
+			node.Right = nil
+		}
+		if node.Left != nil {
+			stack = append(stack, node.Left)
+			node.Left = nil
+		}
+	}
+	return result
+}
