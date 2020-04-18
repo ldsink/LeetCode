d8e3921b29107d721771e5d8a8095e1ee2a081f1
ZHOU Cheng <c.zhou@live.com>
2020-04-18 23:36:13 +0800

    Create 147-insertion-sort-list.go

---
+++ 147-insertion-sort-list.go
@@ -0,0 +1,31 @@
+package main
+
+func insertionSortList(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	var result, node, next *ListNode
+	for node = head; node != nil; node = next {
+		next = node.Next
+		node.Next = nil
+		if result == nil {
+			result = node
+		} else if node.Val <= result.Val {
+			node.Next = result
+			result = node
+		} else {
+			var prev, curr *ListNode
+			for prev, curr = result, result.Next; curr != nil; prev, curr = curr, curr.Next {
+				if node.Val <= curr.Val {
+					prev.Next = node
+					node.Next = curr
+					break
+				}
+			}
+			if curr == nil {
+				prev.Next = node
+			}
+		}
+	}
+	return result
+}
