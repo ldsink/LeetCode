bb321563043fa1c75ec72b2278ccb16af438713b
ZHOU Cheng <c.zhou@live.com>
2020-04-19 19:17:52 +0800

    Update 152-maximum-product-subarray.go

--- 152-maximum-product-subarray.go
+++ 152-maximum-product-subarray.go
@@ -1,20 +1,73 @@
 package main
 
-import "fmt"
+func maxProductInRange(nums []int, start, end, minusCount, first, last int) int {
+	result := nums[start]
+	if minusCount%2 == 0 {
+		product := 1
+		for i := start; i < end; i++ {
+			product *= nums[i]
+		}
+		return product
+	}
+	// single minus
+	if first == last {
+		if start < first {
+			product := 1
+			for i := start; i < first; i++ {
+				product *= nums[i]
+			}
+			if result < product {
+				result = product
+			}
+		}
+		if first+1 < end {
+			product := 1
+			for i := first + 1; i < end; i++ {
+				product *= nums[i]
+			}
+			if result < product {
+				result = product
+			}
+		}
+		if result < nums[first] {
+			result = nums[first]
+		}
+		return result
+	}
+	// odd minus
+	middle := 1
+	for i := first + 1; i < last; i++ {
+		middle *= nums[i]
+	}
+	product := nums[first] * middle
+	for i := start; i < first; i++ {
+		product *= nums[i]
+	}
+	if result < product {
+		result = product
+	}
+	product = middle * nums[last]
+	for i := last + 1; i < end; i++ {
+		product *= nums[i]
+	}
+	if result < product {
+		result = product
+	}
+	return result
+}
 
 func maxProduct(nums []int) int {
 	result := nums[0]
-	var minusCount int
-	start, firstMinusIndex, lastMinusIndex := -1, -1, -1
+	start, minusCount, first, last := -1, 0, -1, -1
 	for index, num := range nums {
 		if start == -1 {
 			start = index
 		}
 		if num < 0 {
 			minusCount++
-			lastMinusIndex = index
-			if firstMinusIndex == -1 {
-				firstMinusIndex = index
+			last = index
+			if first == -1 {
+				first = index
 			}
 		} else if num == 0 {
 			if result < 0 {
@@ -23,118 +76,18 @@ func maxProduct(nums []int) int {
 			if start == index {
 				start = -1
 				continue
-			} else if minusCount%2 == 0 {
-				product := 1
-				for i := start; i < index; i++ {
-					product *= nums[i]
-				}
-				if result < product {
-					result = product
-				}
-			} else if firstMinusIndex == lastMinusIndex {
-				if start < firstMinusIndex {
-					product := 1
-					for i := start; i < firstMinusIndex; i++ {
-						product *= nums[i]
-					}
-					if result < product {
-						result = product
-					}
-				}
-				if firstMinusIndex+1 < index {
-					product := 1
-					for i := firstMinusIndex + 1; i < index; i++ {
-						product *= nums[i]
-					}
-					if result < product {
-						result = product
-					}
-				}
-				if result < nums[firstMinusIndex] {
-					result = nums[firstMinusIndex]
-				}
-			} else {
-				middle := 1
-				for i := firstMinusIndex + 1; i < lastMinusIndex; i++ {
-					middle *= nums[i]
-				}
-				product := nums[firstMinusIndex] * middle
-				for i := start; i < firstMinusIndex; i++ {
-					product *= nums[i]
-				}
-				if result < product {
-					result = product
-				}
-				product = middle * nums[lastMinusIndex]
-				for i := lastMinusIndex + 1; i < index; i++ {
-					product *= nums[i]
-				}
-				if result < product {
-					result = product
-				}
 			}
-			start, firstMinusIndex, lastMinusIndex = -1, -1, -1
-			minusCount = 0
+			if product := maxProductInRange(nums, start, index, minusCount, first, last); result < product {
+				result = product
+			}
+			start, minusCount, first, last = -1, 0, -1, -1
 		}
 	}
 
-	// 处理最后的结尾
 	if start != -1 {
-		if minusCount%2 == 0 {
-			product := 1
-			for i := start; i < len(nums); i++ {
-				product *= nums[i]
-			}
-			if result < product {
-				result = product
-			}
-		} else if firstMinusIndex == lastMinusIndex {
-			if start < firstMinusIndex {
-				product := 1
-				for i := start; i < firstMinusIndex; i++ {
-					product *= nums[i]
-				}
-				if result < product {
-					result = product
-				}
-			}
-			if firstMinusIndex+1 < len(nums) {
-				product := 1
-				for i := firstMinusIndex + 1; i < len(nums); i++ {
-					product *= nums[i]
-				}
-				if result < product {
-					result = product
-				}
-			}
-			if result < nums[firstMinusIndex] {
-				result = nums[firstMinusIndex]
-			}
-		} else {
-			middle := 1
-			for i := firstMinusIndex + 1; i < lastMinusIndex; i++ {
-				middle *= nums[i]
-			}
-			product := nums[firstMinusIndex] * middle
-			for i := start; i < firstMinusIndex; i++ {
-				product *= nums[i]
-			}
-			if result < product {
-				result = product
-			}
-			product = middle * nums[lastMinusIndex]
-			for i := lastMinusIndex + 1; i < len(nums); i++ {
-				product *= nums[i]
-			}
-			if result < product {
-				result = product
-			}
+		if product := maxProductInRange(nums, start, len(nums), minusCount, first, last); result < product {
+			result = product
 		}
 	}
 	return result
-}
-
-func main() {
-	fmt.Println(maxProduct([]int{2, 3, -2, 4}))
-	fmt.Println(maxProduct([]int{-2, 0, 2, -2, -3, -5}))
 }
