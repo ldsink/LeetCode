3715fb712a44176dadc101a5da28a7656889859a
ZHOU Cheng <c.zhou@live.com>
2020-04-20 22:57:27 +0800

    Update 169-majority-element.go

--- 169-majority-element.go
+++ 169-majority-element.go
@@ -1,15 +1,22 @@
 package main
 
 func majorityElement(nums []int) int {
-	var stack []int
-	for _, num := range nums {
-		if len(stack) == 0 {
-			stack = append(stack, num)
-		} else if num != stack[len(stack)-1] {
-			stack = stack[:len(stack)-1]
+	var major, count int
+	for i := 0; i+1 < len(nums); i += 2 {
+		if nums[i] != nums[i+1] {
+			// pass
+		} else if count == 0 {
+			count++
+			major = nums[i]
+		} else if major != nums[i] {
+			count--
 		} else {
-			stack = append(stack, num)
+			count++
 		}
 	}
-	return stack[0]
+	if count > 0 {
+		return major
+	} else {
+		return nums[len(nums)-1]
+	}
 }
