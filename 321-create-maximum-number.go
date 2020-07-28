28ce1866116cc99442ac2178f187b3fcd3e26fe1
ZHOU Cheng <c.zhou@live.com>
2020-07-28 15:44:55 +0800

    Update 321-create-maximum-number.go

--- 321-create-maximum-number.go
+++ 321-create-maximum-number.go
@@ -1,8 +1,5 @@
 package main
 
-var array1, array2 []int
-var cache map[[3]int][]int
-
 func isMax(nums1, nums2 []int) bool {
 	for i := 0; i < len(nums1); i++ {
 		if nums1[i] > nums2[i] {
@@ -14,71 +11,77 @@ func isMax(nums1, nums2 []int) bool {
 	return false
 }
 
-func search(index1, index2, k int) {
-	key := [3]int{index1, index2, k}
-	if _, ok := cache[key]; ok {
-		return
-	}
+func greedyPrune(nums []int, k int) []int {
 	if k == 0 {
-		cache[key] = []int{}
-		return
+		return []int{}
+	} else if len(nums) <= k {
+		return nums
 	}
+	var result []int
+	for i := 0; i < len(nums); i++ {
+		for j := len(result) - 1; j >= 0 && i+(k-j) <= len(nums); j-- {
+			if result[j] >= nums[i] {
+				break
+			}
+			result = result[:j]
+		}
+		if len(result) < k {
+			result = append(result, nums[i])
+		}
+	}
+	return result
+}
 
-	var limit, idx1, idx2 int
-	num1, num2 := -1, -1
-	// get array1 max number
-	limit = len(array1)
-	if k-(len(array2)-index2) > 0 {
-		limit -= k - (len(array2) - index2) - 1
-	}
-	for i := index1; i < limit; i++ {
-		if num1 < array1[i] {
-			num1 = array1[i]
-			idx1 = i
+func greedyMerge(nums1, nums2 []int) []int {
+	var result []int
+	for i, j := 0, 0; i <= len(nums1) && j <= len(nums2); {
+		if i == len(nums1) {
+			return append(result, nums2[j:]...)
+		} else if j == len(nums2) {
+			return append(result, nums1[i:]...)
+		} else if nums1[i] > nums2[j] {
+			result = append(result, nums1[i])
+			i++
+		} else if nums1[i] < nums2[j] {
+			result = append(result, nums2[j])
+			j++
+		} else {
+			l := 1
+			for ; i+l < len(nums1) && j+l < len(nums2) && nums1[i+l] == nums2[j+l]; l++ {
+			}
+			useNum1 := true
+			if i+l == len(nums1) {
+				useNum1 = false
+			} else if j+l == len(nums2) {
+				useNum1 = true
+			} else if nums1[i+l] < nums2[j+l] {
+				useNum1 = false
+			}
+			if useNum1 {
+				result = append(result, nums1[i])
+				i++
+			} else {
+				result = append(result, nums2[j])
+				j++
+			}
 		}
-		if num1 == 9 {
-			break
-		}
 	}
-	// get array2 max number
-	limit = len(array2)
-	if k-(len(array1)-index1) > 0 {
-		limit -= k - (len(array1) - index1) - 1
-	}
-	for i := index2; i < limit; i++ {
-		if num2 < array2[i] {
-			num2 = array2[i]
-			idx2 = i
-		}
-		if num2 == 9 {
-			break
-		}
-	}
-
-	nextA := [3]int{idx1 + 1, index2, k - 1}
-	nextB := [3]int{index1, idx2 + 1, k - 1}
-	if num1 > num2 || index2 == len(array2) {
-		search(idx1+1, index2, k-1)
-		cache[key] = append([]int{num1}, cache[nextA]...)
-		return
-	} else if num1 < num2 || index1 == len(array1) {
-		search(index1, idx2+1, k-1)
-		cache[key] = append([]int{num2}, cache[nextB]...)
-		return
-	}
-	search(idx1+1, index2, k-1)
-	search(index1, idx2+1, k-1)
-	if isMax(cache[nextA], cache[nextB]) {
-		cache[key] = append([]int{num1}, cache[nextA]...)
-	} else {
-		cache[key] = append([]int{num2}, cache[nextB]...)
-	}
+	return result
 }
 
 func maxNumber(nums1 []int, nums2 []int, k int) []int {
-	array1 = nums1
-	array2 = nums2
-	cache = make(map[[3]int][]int)
-	search(0, 0, k)
-	return cache[[3]int{0, 0, k}]
+	nums1 = greedyPrune(nums1, k)
+	nums2 = greedyPrune(nums2, k)
+	result := append(nums1, nums2...)[:k]
+	var idx1 int
+	if k > len(nums2) {
+		idx1 = k - len(nums2)
+	}
+	for i := idx1; i <= k && i <= len(nums1); i++ {
+		current := greedyMerge(greedyPrune(nums1, i), greedyPrune(nums2, k-i))
+		if isMax(current, result) {
+			result = current
+		}
+	}
+	return result
 }
