48073b21e0984a1931c2bc1c750c88b6ec519382
ZHOU Cheng <c.zhou@live.com>
2020-11-21 21:58:50 +0800

    Update 332-reconstruct-itinerary.go

--- 332-reconstruct-itinerary.go
+++ 332-reconstruct-itinerary.go
@@ -4,15 +4,15 @@ import (
 	"sort"
 )
 
-type Result struct {
+type result332 struct {
 	order []string
 }
 
-func dfs(graph map[string][]string, airport string, result *Result) {
+func dfs332(graph map[string][]string, airport string, result *result332) {
 	for ; len(graph[airport]) > 0; {
 		port := graph[airport][0]
 		graph[airport] = graph[airport][1:]
-		dfs(graph, port, result)
+		dfs332(graph, port, result)
 	}
 	result.order = append(result.order, airport)
 }
@@ -25,8 +25,8 @@ func findItinerary(tickets [][]string) []string {
 	for airport, _ := range graph {
 		sort.Strings(graph[airport])
 	}
-	var result Result
-	dfs(graph, "JFK", &result)
+	var result result332
+	dfs332(graph, "JFK", &result)
 	for i, j := 0, len(result.order)-1; i < j; i, j = i+1, j-1 {
 		result.order[i], result.order[j] = result.order[j], result.order[i]
 	}
