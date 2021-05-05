48916eb055aa914afe1c364582ae8696dabe4297
ZHOU Cheng <c.zhou@live.com>
2021-05-05 13:53:57 +0800

    Create 383-ransom-note.go

---
+++ 383-ransom-note.go
@@ -0,0 +1,17 @@
+package main
+
+func canConstruct(ransomNote string, magazine string) bool {
+	var resource, target [26]int
+	for _, r := range magazine {
+		resource[r-'a'] ++
+	}
+	for _, r := range ransomNote {
+		target[r-'a'] ++
+	}
+	for i := 0; i < 26; i++ {
+		if resource[i] < target[i] {
+			return false
+		}
+	}
+	return true
+}
