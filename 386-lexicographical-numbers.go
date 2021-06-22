7554f0a0477974f3eb5d0954495f6c229e1d08c3
ZHOU Cheng <c.zhou@live.com>
2021-06-23 00:25:51 +0800

    Update 386-lexicographical-numbers.go

--- 386-lexicographical-numbers.go
+++ 386-lexicographical-numbers.go
@@ -5,7 +5,7 @@ import (
 	"strconv"
 )
 
-func lexicalOrder(n int) []int {
+func lexicalOrderCheck(n int) []int {
 	var s []string
 	for i := 1; i <= n; i++ {
 		s = append(s, strconv.Itoa(i))
@@ -16,5 +16,28 @@ func lexicalOrder(n int) []int {
 		i, _ := strconv.Atoi(a)
 		result = append(result, i)
 	}
+	return result
+}
+
+func extendNums(num, idx int, plus bool, result []int) int {
+	if num > len(result) {
+		return idx
+	}
+	result[idx] = num
+	idx++
+
+	idx = extendNums(num*10, idx, true, result)
+
+	if plus {
+		for i := 1; (num+i)%10 != 0; i++ {
+			idx = extendNums(num+i, idx, false, result)
+		}
+	}
+	return idx
+}
+
+func lexicalOrder(n int) []int {
+	result := make([]int, n)
+	extendNums(1, 0, true, result)
 	return result
 }
