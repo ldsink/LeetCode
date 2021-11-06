423774d4693b41a89b4ad03883fb58ca6fed2b10
ZHOU Cheng <c.zhou@live.com>
2021-11-06 21:57:09 +0800

    Create 836-rectangle-overlap.go

---
+++ 836-rectangle-overlap.go
@@ -0,0 +1,46 @@
+package main
+
+func isRectangleOverlap(rec1 []int, rec2 []int) bool {
+	// rec1 horizon / rec2 vertical
+	// (rec1[0], rec1[1]) (rec1[2], rec1[1])
+	// (rec1[0], rec1[3]) (rec1[2], rec1[3])
+	// (rec2[0], rec2[1]) (rec2[0], rec2[3])
+	// (rec2[2], rec2[1]) (rec2[2], rec2[3])
+	if rec1[0] < rec2[0] && rec2[0] < rec1[2] && rec2[1] < rec1[1] && rec1[1] < rec2[3] {
+		return true
+	}
+	if rec1[0] < rec2[0] && rec2[0] < rec1[2] && rec2[1] < rec1[1] && rec1[1] < rec2[3] {
+		return true
+	}
+	if rec1[0] < rec2[0] && rec2[0] < rec1[2] && rec2[1] < rec1[3] && rec1[3] < rec2[3] {
+		return true
+	}
+	if rec1[0] < rec2[2] && rec2[2] < rec1[2] && rec2[1] < rec1[3] && rec1[3] < rec2[3] {
+		return true
+	}
+	// rec1 vertical / rec2 horizon
+	// (rec1[0], rec1[1]) (rec1[0], rec1[3])
+	// (rec1[2], rec1[1]) (rec1[2], rec1[3])
+	// (rec2[0], rec2[1]) (rec2[2], rec2[1])
+	// (rec2[0], rec2[3]) (rec2[2], rec2[3])
+	if rec2[0] < rec1[0] && rec1[0] < rec2[2] && rec1[1] < rec2[1] && rec2[1] < rec1[3] {
+		return true
+	}
+	if rec2[0] < rec1[0] && rec1[0] < rec2[2] && rec1[1] < rec2[1] && rec2[1] < rec1[3] {
+		return true
+	}
+	if rec2[0] < rec1[0] && rec1[0] < rec2[2] && rec1[1] < rec2[3] && rec2[3] < rec1[3] {
+		return true
+	}
+	if rec2[0] < rec1[2] && rec1[2] < rec2[2] && rec1[1] < rec2[3] && rec2[3] < rec1[3] {
+		return true
+	}
+	// contain
+	if rec1[0] <= rec2[0] && rec1[1] <= rec2[1] && rec2[2] <= rec1[2] && rec2[3] <= rec1[3] {
+		return true
+	}
+	if rec2[0] <= rec1[0] && rec2[1] <= rec1[1] && rec1[2] <= rec2[2] && rec1[3] <= rec2[3] {
+		return true
+	}
+	return false
+}
