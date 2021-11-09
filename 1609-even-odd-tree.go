397879be945867051748482eb47b372941bcac53
ZHOU Cheng <c.zhou@live.com>
2021-11-09 18:50:18 +0800

    Update 1609-even-odd-tree.go

--- 1609-even-odd-tree.go
+++ 1609-even-odd-tree.go
@@ -5,47 +5,51 @@ func isEvenOddTree(root *TreeNode) bool {
 		return true
 	}
 
-	nodes := []*TreeNode{root}
-	nodeLevels := []int{0}
-	var levelVal []int
-	for i := 0; i < len(nodes); i++ {
-		// 检查当前节点
-		isOdd := nodeLevels[i]%2 != 0
-		if isOdd {
-			if nodes[i].Val%2 != 0 {
+	evenNodes := []*TreeNode{root}
+	oddNodes := []*TreeNode{}
+
+	for len(oddNodes) > 0 || len(evenNodes) > 0 {
+		// 检查偶数层
+		for i := 0; i < len(evenNodes); i++ {
+			if evenNodes[i].Val%2 == 0 {
 				return false
 			}
-		} else {
-			if nodes[i].Val%2 == 0 {
+			if evenNodes[i].Left != nil {
+				oddNodes = append(oddNodes, evenNodes[i].Left)
+			}
+			if evenNodes[i].Right != nil {
+				oddNodes = append(oddNodes, evenNodes[i].Right)
+			}
+		}
+		// 偶数层严格递减
+		for i := 1; i < len(evenNodes); i++ {
+			if evenNodes[i].Val <= evenNodes[i-1].Val {
 				return false
 			}
 		}
-		// 比较当前层级的值
-		if len(levelVal) < nodeLevels[i]+1 {
-			// 当前层级的第一个节点
-			levelVal = append(levelVal, nodes[i].Val)
-		} else {
-			// 检查是不是按照顺序
-			if isOdd {
-				if levelVal[nodeLevels[i]] <= nodes[i].Val {
-					return false
-				}
-			} else {
-				if levelVal[nodeLevels[i]] >= nodes[i].Val {
-					return false
-				}
+		// 清空偶数层
+		evenNodes = []*TreeNode{}
+
+		// 检查奇数层
+		for i := 0; i < len(oddNodes); i++ {
+			if oddNodes[i].Val%2 != 0 {
+				return false
 			}
-			levelVal[nodeLevels[i]] = nodes[i].Val
+			if oddNodes[i].Left != nil {
+				evenNodes = append(evenNodes, oddNodes[i].Left)
+			}
+			if oddNodes[i].Right != nil {
+				evenNodes = append(evenNodes, oddNodes[i].Right)
+			}
 		}
-		// 添加下一层
-		if nodes[i].Left != nil {
-			nodes = append(nodes, nodes[i].Left)
-			nodeLevels = append(nodeLevels, nodeLevels[i]+1)
+		// 奇数层严格递减
+		for i := 1; i < len(oddNodes); i++ {
+			if oddNodes[i].Val >= oddNodes[i-1].Val {
+				return false
+			}
 		}
-		if nodes[i].Right != nil {
-			nodes = append(nodes, nodes[i].Right)
-			nodeLevels = append(nodeLevels, nodeLevels[i]+1)
-		}
+		// 清空奇数层
+		oddNodes = []*TreeNode{}
 	}
 	return true
 }
