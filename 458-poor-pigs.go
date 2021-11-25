ebc66132854a4e737073eab1d4aef540eb739f6e
ZHOU Cheng <c.zhou@live.com>
2021-11-25 23:34:13 +0800

    Create 458-poor-pigs.go

---
+++ 458-poor-pigs.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"math"
+)
+
+func poorPigs(buckets int, minutesToDie int, minutesToTest int) int {
+	return int(math.Ceil(math.Log(float64(buckets)) / math.Log(float64(minutesToTest/minutesToDie))))
+}
