dcd7913d9c9789283a3e82d810976b4278525590
ZHOU Cheng <c.zhou@live.com>
2021-12-14 14:11:05 +0800

    Update 630-course-schedule-iii.go

--- 630-course-schedule-iii.go
+++ 630-course-schedule-iii.go
@@ -8,16 +8,17 @@ func scheduleCourse(courses [][]int) int {
 	if len(courses) == 0 {
 		return 0
 	}
-	// 按照结束时间逆序排列，如果结束时间相同，把课时短的放前面
+	// 按照结束时间顺序排序
 	sort.Slice(courses, func(i, j int) bool {
-		return courses[i][1] > courses[j][1] || (courses[i][1] == courses[j][1] && courses[i][0] < courses[j][0])
+		return courses[i][1] < courses[j][1]
 	})
-	// 动态规划，result[i] 表示第 i 天起可以上的最多的课程
-	result := make([]int, courses[0][1]+2)
-	for _, course := range courses {
-		for i, j := 1, course[1]-course[0]+1; i <= j; i++ {
-			if result[i] < 1+result[i+course[0]] {
-				result[i] = 1 + result[i+course[0]]
+	// 动态规划，result[i] 表示第 i 天起可以上的最多的课程。课程从后往前递归处理。
+	result := make([]int, courses[len(courses)-1][1]+2)
+	for idx := len(courses) - 1; idx >= 0; idx-- {
+		duration, lastDay := courses[idx][0], courses[idx][1]
+		for i, j := 1, lastDay-duration+1; i <= j; i++ {
+			if result[i] < 1+result[i+duration] {
+				result[i] = 1 + result[i+duration]
 			}
 		}
 	}
