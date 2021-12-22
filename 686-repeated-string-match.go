3daeff1fc7576e8e3d95a008cfb1fc4901540478
ZHOU Cheng <c.zhou@live.com>
2021-12-22 21:01:34 +0800

    Update 686-repeated-string-match.go

--- 686-repeated-string-match.go
+++ 686-repeated-string-match.go
@@ -3,20 +3,21 @@ package main
 import "strings"
 
 func repeatedStringMatch(a string, b string) int {
-	var count int
-	var c string
-	for count*len(a) < len(b) {
-		c += a
-		count++
+	// 首先构造出一个最长的串
+	l := len(b) / len(a)
+	if len(b)%len(a) > 0 {
+		l++
 	}
-	if strings.Contains(c, b) {
-		return count
+	max := l * 2
+	var arr []string
+	for i := 0; i < max; i++ {
+		arr = append(arr, a)
 	}
-	for end := count * 2; count <= end; {
-		c += a
-		count++
-		if strings.Contains(c, b) {
-			return count
+	c := strings.Join(arr, "")
+	// 然后直接枚举长度
+	for i := len(a); i <= len(c); i = i + len(a) {
+		if strings.Contains(c[:i], b) {
+			return i / len(a)
 		}
 	}
 	return -1
