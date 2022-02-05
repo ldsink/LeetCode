d4d937cc0d6f6eeeb7cd0651d58f84d1b88d6055
ZHOU Cheng <c.zhou@live.com>
2022-02-05 13:58:04 +0800

    Update 1219-path-with-maximum-gold.go

--- 1219-path-with-maximum-gold.go
+++ 1219-path-with-maximum-gold.go
@@ -3,7 +3,6 @@ package main
 func collectGold(x, y int, grid [][]int, collected map[[2]int]bool) int {
 	collected[[2]int{x, y}] = true
 	var gold int
-
 	for _, d := range [4][2]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} {
 		i, j := x+d[0], y+d[1]
 		if i < 0 || i == len(grid) || j < 0 || j == len(grid[0]) {
@@ -23,11 +22,9 @@ func collectGold(x, y int, grid [][]int, collected map[[2]int]bool) int {
 func getMaximumGold(grid [][]int) int {
 	// 最多 25 个单元格中有黄金。
 	collected := make(map[[2]int]bool)
-	m := len(grid)
-	n := len(grid[1])
 	var result int
-	for i := 0; i < m; i++ {
-		for j := 0; j < n; j++ {
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
 			if grid[i][j] > 0 {
 				if gold := collectGold(i, j, grid, collected); result < gold {
 					result = gold
