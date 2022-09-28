9904b3cac3dbbc8ae9d60345674e5ea44367d732
ZHOU Cheng <c.zhou@live.com>
2022-09-29 00:51:42 +0800

    Update 313-super-ugly-number.go

--- 313-super-ugly-number.go
+++ 313-super-ugly-number.go
@@ -4,16 +4,13 @@ import (
 	"container/heap"
 )
 
-type UglyNumber struct {
-	num, base, idx int
-}
-type MinHeap []UglyNumber
+type MinHeap [][3]int // num, base, index
 
 func (h *MinHeap) Len() int           { return len(*h) }
-func (h *MinHeap) Less(i, j int) bool { return (*h)[i].num < (*h)[j].num }
+func (h *MinHeap) Less(i, j int) bool { return (*h)[i][0] < (*h)[j][0] }
 func (h *MinHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
 func (h *MinHeap) Push(x interface{}) {
-	*h = append(*h, x.(UglyNumber))
+	*h = append(*h, x.([3]int))
 }
 func (h *MinHeap) Pop() interface{} {
 	x := (*h)[len(*h)-1]
@@ -27,22 +24,22 @@ func nthSuperUglyNumber(n int, primes []int) int {
 	}
 	inHeap := make(map[int]bool)
 	h := &MinHeap{}
-	heap.Push(h, UglyNumber{num: primes[0], base: 1, idx: 0})
+	heap.Push(h, [3]int{primes[0], 1, 0})
 	for i := 2; ; {
-		curr := heap.Pop(h).(UglyNumber)
-		if inHeap[curr.num] {
-			if curr.idx+1 < len(primes) {
-				heap.Push(h, UglyNumber{num: curr.base * primes[curr.idx+1], base: curr.base, idx: curr.idx + 1})
+		curr := heap.Pop(h).([3]int)
+		if inHeap[curr[0]] {
+			if curr[2]+1 < len(primes) {
+				heap.Push(h, [3]int{curr[1] * primes[curr[2]+1], curr[1], curr[2] + 1})
 			}
 			continue
 		}
 		if i == n {
-			return curr.num
+			return curr[0]
 		}
-		inHeap[curr.num] = true
-		heap.Push(h, UglyNumber{num: curr.num * primes[0], base: curr.num, idx: 0})
-		if curr.idx+1 < len(primes) {
-			heap.Push(h, UglyNumber{num: curr.base * primes[curr.idx+1], base: curr.base, idx: curr.idx + 1})
+		inHeap[curr[0]] = true
+		heap.Push(h, [3]int{curr[0] * primes[0], curr[0], 0})
+		if curr[2]+1 < len(primes) {
+			heap.Push(h, [3]int{curr[1] * primes[curr[2]+1], curr[1], curr[2] + 1})
 		}
 		i++
 	}
