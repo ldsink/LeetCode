0edfdd136979b6cb3066c283e23e94da64ca1612
ZHOU Cheng <c.zhou@live.com>
2022-10-06 19:45:56 +0800

    Update 927-three-equal-parts.go

--- 927-three-equal-parts.go
+++ 927-three-equal-parts.go
@@ -19,8 +19,30 @@ func threeEqualParts(arr []int) []int {
 	//    zero2(一段连续的 0),
 	//    three[len(arr) - l, len(arr)]
 	// ]
-	l, zero1, zero2 := (len(arr)-zeroOffset)/3, 0, 0
-	// 首先初始化 zero1 和 zero2，此时不包含长度为 l 的情况
+	l := (len(arr) - zeroOffset) / 3
+	one1, one2, one3, oneInit := 0, 0, 0, false // 初始化三段区间 1 的数量，用于剪枝
+	for i, j := zeroOffset, zeroOffset+l; i < j; i++ {
+		if arr[i] == 1 {
+			one1++
+		}
+	}
+	for i, j := zeroOffset+l, len(arr)-l; i < j; i++ {
+		if arr[i] == 1 {
+			one2++
+		}
+	}
+	for i := len(arr) - l; i < len(arr); i++ {
+		if arr[i] == 1 {
+			one3++
+		}
+	}
+	impossible := []int{-1, -1}
+	if (one1+one2+one3)%3 != 0 { // 1 的数量无法整除 3，肯定不合法
+		return impossible
+	} else if zeroOffset+one1+one2+one3 == len(arr) { // 前面全是 0，后面全是 1
+		return []int{zeroOffset + l - 1, len(arr) - l}
+	}
+	zero1, zero2 := 0, 0 // 初始化 zero1 和 zero2 的值，此时不包含长度为 l 的情况
 	for i := zeroOffset + l + 1; i < len(arr); i++ {
 		if arr[i] == 1 {
 			break
@@ -33,23 +55,33 @@ func threeEqualParts(arr []int) []int {
 		}
 		zero2++
 	}
-	impossible := []int{-1, -1}
 	for ; l > 0; l-- {
 		// 进入后先更新当长度为 l 的情况下，两段 0 区间的情况
 		if arr[zeroOffset+l] == 0 {
 			zero1++
 		} else {
 			zero1 = 0
+			if oneInit {
+				one1--
+				one2++
+			}
 		}
 		if arr[len(arr)-l-1] == 0 {
 			zero2++
 		} else {
 			zero2 = 0
+			if oneInit {
+				one2++
+				one3--
+			}
+		}
+		if !oneInit {
+			oneInit = true
 		}
 		if zeroOffset+3*l+zero1+zero2 < len(arr) { // 排除掉不可能的情况
 			return impossible
 		}
-		if arr[len(arr)-l] == 0 { // 第三段有效位置不满足条件，前往下一个 l 进行计算
+		if arr[len(arr)-l] == 0 || one1 != one2 || one2 != one3 { // 第三段有效位置不满足条件，或者 1 的数量不一致
 			continue
 		}
 		// 基于两段 0 区间进行剪枝搜索
@@ -62,7 +94,6 @@ func threeEqualParts(arr []int) []int {
 					continue
 				}
 				success = false
-				break
 			}
 			if success {
 				return []int{zeroOffset + l - 1, part2Start + l}
