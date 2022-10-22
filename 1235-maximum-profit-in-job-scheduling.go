ff9a2868237249f4c6ceed614d2e9e126948c408
ZHOU Cheng <c.zhou@live.com>
2022-10-22 16:53:37 +0800

    Update 1235-maximum-profit-in-job-scheduling.go

--- 1235-maximum-profit-in-job-scheduling.go
+++ 1235-maximum-profit-in-job-scheduling.go
@@ -7,38 +7,34 @@ import (
 
 func jobScheduling(startTime []int, endTime []int, profit []int) int {
 	type job struct {
-		end, profit int
+		start, end, profit, maxProfit int
 	}
-	jobMap := make(map[int][]job)
+	var jobs []job
 	for i := 0; i < len(startTime); i++ {
-		jobMap[startTime[i]] = append(jobMap[startTime[i]], job{end: endTime[i], profit: profit[i]})
+		jobs = append(jobs, job{start: startTime[i], end: endTime[i], profit: profit[i]})
 	}
-	nodes := []int{math.MaxInt}
-	for start := range jobMap {
-		nodes = append(nodes, start)
-	}
-	sort.Ints(nodes)
-	maxProfit := make([]int, len(nodes))
-	for i := 0; i < len(maxProfit)-1; i++ {
-		if i > 0 && maxProfit[i] < maxProfit[i-1] {
-			maxProfit[i] = maxProfit[i-1]
+	sort.Slice(jobs, func(i, j int) bool {
+		return jobs[i].start < jobs[j].start || (jobs[i].start == jobs[j].start && jobs[i].end < jobs[j].end)
+	})
+	jobs = append(jobs, job{start: math.MaxInt})
+	for i := 0; i < len(jobs)-1; i++ {
+		if i > 0 && jobs[i].maxProfit < jobs[i-1].maxProfit {
+			jobs[i].maxProfit = jobs[i-1].maxProfit
 		}
-		for _, j := range jobMap[nodes[i]] {
-			// 找到下一个节点位置
-			next, end := i+1, len(nodes)-1
-			for next != end {
-				middle := (next + end) >> 1
-				if j.end > nodes[middle] {
-					next = middle + 1
-				} else {
-					end = middle
-				}
-			}
-			// 更新下一个节点的最大收益
-			if maxProfit[next] < maxProfit[i]+j.profit {
-				maxProfit[next] = maxProfit[i] + j.profit
+		// 找到下一个节点位置
+		next, end := i+1, len(jobs)-1
+		for next != end {
+			middle := (next + end) >> 1
+			if jobs[i].end > jobs[middle].start {
+				next = middle + 1
+			} else {
+				end = middle
 			}
 		}
+		// 更新下一个节点的最大收益
+		if jobs[next].maxProfit < jobs[i].maxProfit+jobs[i].profit {
+			jobs[next].maxProfit = jobs[i].maxProfit + jobs[i].profit
+		}
 	}
-	return maxProfit[len(nodes)-1]
+	return jobs[len(jobs)-1].maxProfit
 }
