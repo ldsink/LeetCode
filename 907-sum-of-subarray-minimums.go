e4f560360065f88da603d1fa424c09534953bf48
ZHOU Cheng <c.zhou@live.com>
2022-10-28 21:57:41 +0800

    Update 907-sum-of-subarray-minimums.go

--- 907-sum-of-subarray-minimums.go
+++ 907-sum-of-subarray-minimums.go
@@ -4,13 +4,14 @@ func sumSubarrayMins(arr []int) int {
 	const mod = 1000000007
 	var left, right, result int
 	for i := 0; i < len(arr); i++ {
-		for left = i - 1; left >= 0 && arr[left] >= arr[i]; left-- {
+		if i == 0 || arr[i] > arr[i-1] {
+			left = i - 1
 		}
-		left = i - left - 1
+		for ; left >= 0 && arr[left] >= arr[i]; left-- {
+		}
 		for right = i + 1; right < len(arr) && arr[right] > arr[i]; right++ {
 		}
-		right = right - i - 1
-		result += arr[i] * (left + 1) * (right + 1)
+		result += arr[i] * (i - left) * (right - i)
 		result %= mod
 	}
 	return result
