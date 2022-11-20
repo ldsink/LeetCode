bc68986340222490087c94142a128ddf8a6856cf
ZHOU Cheng <c.zhou@live.com>
2022-11-20 13:30:08 +0800

    Update 6242-closest-nodes-queries-in-a-binary-search-tree.go

--- 6242-closest-nodes-queries-in-a-binary-search-tree.go
+++ 6242-closest-nodes-queries-in-a-binary-search-tree.go
@@ -1,5 +1,7 @@
 package main
 
+import "sort"
+
 /**
  * Definition for a binary tree node.
  * type TreeNode struct {
@@ -9,37 +11,55 @@ package main
  * }
  */
 func closestNodes(root *TreeNode, queries []int) [][]int {
-	var findMin, findMax func(*TreeNode, int) int
 	const failed int = -1
-	findMin = func(node *TreeNode, val int) int {
-		if node == nil {
-			return failed
+	// 把树转换成数组
+	var array []int
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		array = append(array, queue[0].Val)
+		if queue[0].Left != nil {
+			queue = append(queue, queue[0].Left)
 		}
-		if val == node.Val {
-			return val
-		} else if val < node.Val {
-			return findMin(node.Left, val)
-		} else if next := findMin(node.Right, val); next != failed {
-			return next
+		if queue[0].Right != nil {
+			queue = append(queue, queue[0].Right)
 		}
-		return node.Val
+		queue = queue[1:]
 	}
-	findMax = func(node *TreeNode, val int) int {
-		if node == nil {
+	sort.Ints(array)
+	var findMin, findMax func(int) int
+	findMin = func(val int) int {
+		if val < array[0] {
 			return failed
 		}
-		if val == node.Val {
-			return val
-		} else if val > node.Val {
-			return findMax(node.Right, val)
-		} else if next := findMax(node.Left, val); next != failed {
-			return next
+		start, end := 0, len(array)-1
+		for start != end {
+			middle := (start + end) >> 1
+			if array[middle+1] <= val {
+				start = middle + 1
+			} else {
+				end = middle
+			}
 		}
-		return node.Val
+		return array[start]
+	}
+	findMax = func(val int) int {
+		if val > array[len(array)-1] {
+			return failed
+		}
+		start, end := 0, len(array)-1
+		for start != end {
+			middle := (start + end) >> 1
+			if array[middle] >= val {
+				end = middle
+			} else {
+				start = middle + 1
+			}
+		}
+		return array[start]
 	}
 	var result [][]int
 	for _, query := range queries {
-		result = append(result, []int{findMin(root, query), findMax(root, query)})
+		result = append(result, []int{findMin(query), findMax(query)})
 	}
 	return result
 }
