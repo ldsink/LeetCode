701a9f30b2dcaf8d1c1bdd1c7dae17da6c0f32dd
ZHOU Cheng <c.zhou@live.com>
2022-12-12 10:45:19 +0800

    Update 1781-sum-of-beauty-of-all-substrings.go

--- 1781-sum-of-beauty-of-all-substrings.go
+++ 1781-sum-of-beauty-of-all-substrings.go
@@ -5,30 +5,32 @@ import "math"
 func beautySum(s string) int {
 	const L = 26
 	const A = 'a'
-	getBeauty := func(start, end int) int {
+	getBeauty := func(start int) (sum int) {
 		count := make([]int, L)
-		for i := start; i < end; i++ {
-			count[s[i]-A]++
+		var max, min int
+		for end := start; end < len(s); end++ {
+			idx := s[end] - A
+			count[idx]++
+			if max < count[idx] {
+				max = count[idx]
+			}
+			if min > count[idx] { // 新加入的就是最小的
+				min = count[idx]
+			} else if min == count[idx]-1 { // 这次更新可能造成 min 变化，重新计算一下 min
+				min = math.MaxInt
+				for i := 0; i < L; i++ {
+					if count[i] != 0 && min > count[i] {
+						min = count[i]
+					}
+				}
+			}
+			sum += max - min
 		}
-		max, min := 0, math.MaxInt
-		for i := 0; i < L; i++ {
-			if count[i] == 0 {
-				continue
-			}
-			if max < count[i] {
-				max = count[i]
-			}
-			if min > count[i] {
-				min = count[i]
-			}
-		}
-		return max - min
+		return
 	}
 	var result int
 	for i := 0; i < len(s); i++ {
-		for j := i + 2; j <= len(s); j++ {
-			result += getBeauty(i, j)
-		}
+		result += getBeauty(i)
 	}
 	return result
 }
