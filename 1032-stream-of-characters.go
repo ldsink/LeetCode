59ca00fd4fb4cac09327ada97c7d7b14179250f7
ZHOU Cheng <c.zhou@live.com>
2023-03-25 20:52:47 +0800

    Update 1032-stream-of-characters.go

--- 1032-stream-of-characters.go
+++ 1032-stream-of-characters.go
@@ -1,34 +1,50 @@
 package main
 
+type TrieNode struct {
+	children map[byte]*TrieNode
+	end      bool
+}
+
 type StreamChecker struct {
-	words   [][]rune
-	letters []rune
+	root    *TrieNode
+	letters []byte
+}
+
+func addTrieNode(root *TrieNode, word string) {
+	for node, i := root, len(word)-1; i >= 0; i-- {
+		if node.children == nil {
+			node.children = make(map[byte]*TrieNode)
+		}
+		if _, ok := node.children[word[i]]; !ok {
+			node.children[word[i]] = &TrieNode{}
+		}
+		node = node.children[word[i]]
+		if i == 0 {
+			node.end = true
+		}
+	}
 }
 
 func Constructor(words []string) StreamChecker {
-	var w [][]rune
+	root := &TrieNode{}
 	for _, word := range words {
-		w = append(w, []rune(word))
+		addTrieNode(root, word)
 	}
-	return StreamChecker{words: w}
+	return StreamChecker{root: root}
 }
 
 func (this *StreamChecker) Query(letter byte) bool {
-	this.letters = append(this.letters, rune(letter))
+	this.letters = append(this.letters, letter)
 	if len(this.letters) > 2000 {
 		this.letters = this.letters[len(this.letters)-200:]
 	}
-	var i, j int
-	for _, word := range this.words {
-		if len(word) > len(this.letters) {
-			continue
+	i := len(this.letters) - 1
+	for node := this.root; i >= 0; i-- {
+		if _, ok := node.children[this.letters[i]]; !ok {
+			return false
 		}
-		for i, j = 0, len(this.letters)-len(word); i < len(word); i++ {
-			if word[i] != this.letters[j+i] {
-				break
-			}
-		}
-		if i == len(word) {
+		node = node.children[this.letters[i]]
+		if node.end {
 			return true
 		}
 	}
