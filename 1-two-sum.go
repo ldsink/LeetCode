f48ce4714ea19d49cd559f23a8a53e8a584a40f4
ZHOU Cheng <c.zhou@live.com>
2023-07-01 22:34:14 +0800

    Update 1-two-sum.go

--- 1-two-sum.go
+++ 1-two-sum.go
@@ -1,95 +1,25 @@
 package main
 
-type Number struct {
-	value int
-	index int
-}
-
-func mergeSort(array, sorted []Number, left, right int) {
-	if left >= right {
-		return
-	}
-	middle := (left + right) >> 1
-	mergeSort(array, sorted, left, middle)
-	mergeSort(array, sorted, middle+1, right)
-
-	leftStart, rightStart := left, middle+1
-	for position := left; leftStart <= middle || rightStart <= right; position++ {
-		if leftStart <= middle && rightStart <= right {
-			if array[leftStart].value > array[rightStart].value {
-				sorted[position] = array[rightStart]
-				rightStart++
-			} else {
-				sorted[position] = array[leftStart]
-				leftStart++
-			}
-		} else if leftStart <= middle {
-			sorted[position] = array[leftStart]
-			leftStart++
-		} else {
-			sorted[position] = array[rightStart]
-			rightStart++
-		}
-	}
-
-	for position := left; position <= right; position++ {
-		array[position] = sorted[position]
-	}
-}
-
-const failed = -1
-
-func findNumber(number int, nums []Number, start, end int) int {
-	if start == end {
-		if nums[start].value == number {
-			return start
-		}
-		return failed
-	}
-	middle := (start + end) >> 1
-	if nums[middle].value == number {
-		return middle
-	} else if nums[middle].value > number {
-		end = middle - 1
-		if end < start {
-			return failed
-		}
-		return findNumber(number, nums, start, end)
-	} else {
-		start = middle + 1
-		if start > end {
-			return failed
-		}
-		return findNumber(number, nums, start, end)
-	}
-}
+import "sort"
 
 func twoSum(nums []int, target int) []int {
-	length := len(nums)
-	array := make([]Number, length)
-	sorted := make([]Number, length)
-	for i, _ := range array {
-		array[i] = Number{index: i, value: nums[i]}
+	type Number struct {
+		val, idx int
 	}
-
-	mergeSort(array, sorted, 0, length-1)
-
-	start, end := 0, length-1
-	for ; array[start].value+array[end].value > target; end-- {
+	var array []Number
+	for idx, val := range nums {
+		array = append(array, Number{idx: idx, val: val})
 	}
-	for ; array[start].value+array[end].value < target; start++ {
-	}
-
-	for ; ; start++ {
-		rest := target - array[start].value
-		idx := findNumber(rest, array, start+1, end)
-		if idx != failed {
-			end = idx
-			break
+	sort.Slice(array, func(i, j int) bool {
+		return array[i].val < array[j].val || (array[i].val == array[j].val && array[i].idx < array[j].idx)
+	})
+	for start, end := 0, len(nums)-1; ; start++ {
+		for ; array[start].val+array[end].val > target && start < end-1; end-- {
+		}
+		for ; array[start].val+array[end].val < target && end+1 < len(nums); end++ {
+		}
+		if array[start].val+array[end].val == target {
+			return []int{array[start].idx, array[end].idx}
 		}
 	}
-	if array[start].index > array[end].index {
-		return []int{array[end].index, array[start].index}
-	}
-	return []int{array[start].index, array[end].index}
 }
