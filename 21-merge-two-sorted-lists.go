7e685e4f518b0911f327bb32fc4b03389fe048fb
ZHOU Cheng <c.zhou@live.com>
2023-08-05 20:11:53 +0800

    Update 21-merge-two-sorted-lists.go

--- 21-merge-two-sorted-lists.go
+++ 21-merge-two-sorted-lists.go
@@ -6,30 +6,30 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	} else if l2 == nil {
 		return l1
 	}
-	var result *ListNode
-	if (*l1).Val > (*l2).Val {
-		result = l2
-		l2 = (*l2).Next
+
+	var head, tail *ListNode
+	if l1.Val > l2.Val {
+		head = l2
+		l2 = l2.Next
 	} else {
-		result = l1
-		l1 = (*l1).Next
+		head = l1
+		l1 = l1.Next
 	}
-	current := result
-	for l1 != nil && l2 != nil {
-		if (*l1).Val > (*l2).Val {
-			(*current).Next = l2
-			current = l2
-			l2 = (*l2).Next
+	for tail = head; l1 != nil && l2 != nil; {
+		if l1.Val > l2.Val {
+			tail.Next = l2
+			tail = l2
+			l2 = l2.Next
 		} else {
-			(*current).Next = l1
-			current = l1
-			l1 = (*l1).Next
+			tail.Next = l1
+			tail = l1
+			l1 = l1.Next
 		}
 	}
 	if l1 != nil {
-		(*current).Next = l1
+		tail.Next = l1
 	} else if l2 != nil {
-		(*current).Next = l2
+		tail.Next = l2
 	}
-	return result
+	return head
 }
