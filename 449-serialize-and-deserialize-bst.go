7b2ac7c1ce2607f61b80b2ce10af9ac9578ca7f2
ZHOU Cheng <c.zhou@live.com>
2023-09-04 23:50:31 +0800

    Update 449-serialize-and-deserialize-bst.go

--- 449-serialize-and-deserialize-bst.go
+++ 449-serialize-and-deserialize-bst.go
@@ -18,7 +18,7 @@ func (this *Codec) serialize(root *TreeNode) string {
 	if root == nil {
 		return ""
 	}
-	//	val,leftLength,leftData,rightLength,rightData
+	// val,leftLength,leftData,rightLength,rightData
 	leftData := this.serialize(root.Left)
 	rightData := this.serialize(root.Right)
 	return fmt.Sprintf("%d,%d,%s%s", root.Val, len(leftData), leftData, rightData)
@@ -29,11 +29,10 @@ func (this *Codec) deserialize(data string) *TreeNode {
 	if len(data) == 0 {
 		return nil
 	}
-	parts := strings.SplitN(data, ",", 2)
+	parts := strings.SplitN(data, ",", 3)
 	val, _ := strconv.Atoi(parts[0])
-	parts = strings.SplitN(parts[1], ",", 2)
-	leftLength, _ := strconv.Atoi(parts[0])
-	left := this.deserialize(parts[1][:leftLength])
-	right := this.deserialize(parts[1][leftLength:])
+	leftLength, _ := strconv.Atoi(parts[1])
+	left := this.deserialize(parts[2][:leftLength])
+	right := this.deserialize(parts[2][leftLength:])
 	return &TreeNode{Val: val, Left: left, Right: right}
 }
